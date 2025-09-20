b9bf28cc376686c1fc9c4b3ca35c9ac6d0948056
AmosBarSinai <amosbarsinai@gmail.com>
2025-09-20 22:39:58 +0300

    Updated build.py to be cleaner, more Pythonic, and less error-prone.

--- build.py
+++ build.py
@@ -5,30 +5,63 @@ import os
 import shlex
 import sys
 
+from pathlib import Path
+
+if sys.prefix == sys.base_prefix:
+    print("It looks like you are not in a Python virtual environment. It's recommended to use one - Smort is still in heavy development.")
+    while True:
+        answer = input("Do you want to continue anyway? [y/N]\n>> ").strip().lower()
+        if answer in ["y", "yes"]:
+            break
+        elif answer in ["n", "no", ""]:
+            print("Exiting.")
+            sys.exit(0)
+        else:
+            print("Please answer 'y' or 'n'.")
+
+
 NINJA_LIB_BUILD_CMDS = [
     ["ninja"],
     ["sudo", "ninja", "install"]
 ]
 
 PY_LIB_INSTALL_CMDS = [
-    ["pip", "install", ".."]
+    [sys.executable, "-m", "pip", "install", ".."]
 ]
 
 def log_build(message, built=False):
-    print(f"\033[1;33m{ {False: "BUILDING", True: "BUILT"}[built]}\033[0m {message}")
+    status = "BUILT" if built else "BUILDING"
+    print(f"\033[1;33m{status}\033[0m {message}")
 
 def log_cmd(cmd, finished=False):
-    print(f"\033[1;35m{ {False: "EXECUTING", True: "EXECUTED"}[finished]}\033[0m {cmd}")
+    status = "EXECUTED" if finished else "EXECUTING"
+    print(f"\033[1;35m{status}\033[0m {cmd}")
 
-os.chdir(os.path.join(os.path.dirname(__file__), "build"))
 
-log_build("smort")
-for command in NINJA_LIB_BUILD_CMDS + PY_LIB_INSTALL_CMDS:
-    log_cmd(shlex.join(command))
-    exit_code = subprocess.run(command).returncode
-    if exit_code != 0:
-        print(f"Command `{command}` failed with exit code {exit_code}. Exiting.")
-        sys.exit(exit_code)
+build_dir = Path(__file__).parent / "build"
+if not build_dir.is_dir():
+    print(f"Error: build directory {build_dir} does not exist.")
+    print("Please report this error at https://github.com/AmosBarSinai/smort/issues - the build/ directory should be created once ninja has been run.")
+    sys.exit(1)
+os.chdir(build_dir)
+
+def run_cmd(cmd):
+    log_cmd(shlex.join(cmd))
+    try:
+        subprocess.run(cmd, check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Command `{shlex.join(cmd)}` failed with exit code {e.returncode}. Exiting.")
+        sys.exit(e.returncode)
     else:
-        log_cmd(shlex.join(command), finished=True)
-log_build("smort", built=True)
+        log_cmd(shlex.join(cmd), finished=True)
+
+log_build("Smort C++ Shared Object")
+for cmd in NINJA_LIB_BUILD_CMDS:
+    run_cmd(cmd)
+log_build("Smort C++ Shared Object", built=True)
+
+log_build("Smort Python Package")
+for command in PY_LIB_INSTALL_CMDS:
+    run_cmd(command)
+log_build("Smort Python Package", built=True)
+
