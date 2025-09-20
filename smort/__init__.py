4251c90280dde3a4ba987d1eae9a93bffd8d2b8c
AmosBarSinai <amosbarsinai@gmail.com>
2025-09-20 09:59:24 +0300

    Added a nice comment in the init stub.

--- smort/__init__.py
+++ smort/__init__.py
@@ -1,1 +1,9 @@
+"""Smort is a simple and fast multimedia library, designed to make
+the transition into graphics easier for beginners.
+
+It provides intuitive APIs for creating windows, drawing shapes, handling
+input, and working with images and sounds. Smort aims to be lightweight,
+easy to install, and suitable for educational purposes or rapid prototyping.
+"""
+
 from ._smort import *
